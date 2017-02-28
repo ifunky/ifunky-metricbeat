e127725b95250065743489b140e3fe36e1d5d571
Dan <dan@ifunky.net>
2017-02-28 21:42:59 +0000

    Install service after unzipping

--- manifests/install/windows.pp
+++ manifests/install/windows.pp
@@ -1,9 +1,3 @@
-# Role class
-#
-# @example when declaring the class
-#   class { 'role' }
-#
-# @author Dan
 class metricbeat::install::windows () {
 
   $filename = regsubst($metricbeat::download_url, '^https?.*\/([^\/]+)\.[^.].*', '\1')
@@ -20,12 +14,19 @@ class metricbeat::install::windows () {
     proxy       => $metricbeat::proxy_address,
   }
 
-  exec { "unzip ${filename}":
+  exec { "Unzip Metricbeat":
     command  => "Add-Type -Assembly System.IO.Compression.FileSystem;\$zip = [IO.Compression.ZipFile]::OpenRead('${metricbeat::tmp_dir}/${filename}.zip');\$zip.Entries | where {\$_.Name -ne ''} | foreach {[System.IO.Compression.ZipFileExtensions]::ExtractToFile(\$_, \"${metricbeat::install_dir}\\$(\$_.Name)\", \$true)};\$zip.Dispose()",
     creates  => "${metricbeat::install_dir}/metricbeat.exe",
     logoutput => true,
     provider => powershell,
-    require  => [ File[$metricbeat::install_dir], Remote_file["${metricbeat::tmp_dir}/${filename}.zip"],
-    ],
+    require  => [ File[$metricbeat::install_dir], Remote_file["${metricbeat::tmp_dir}/${filename}.zip"] ],
+  }
+
+  exec { "Install Metricbeat service":
+    cwd      => $metricbeat::install_dir,
+    command  => './install-service-metricbeat.ps1',
+    onlyif   => 'if(Get-WmiObject -Class Win32_Service -Filter "Name=\'metricbeat\'") { exit 1 } else {exit 0 }',
+    provider =>  powershell,
+    require  => Exec['Unzip Metricbeat'],
   }
 }
